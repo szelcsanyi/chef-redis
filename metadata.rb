50dffa837bc981dcd1319e72fc42238ddee157a4
szelcsanyi <szelcsanyi.gabor@gmail.com>
2015-12-29 07:44:04 +0100

    cron dependency fix

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,11 @@ license          'MIT'
 description      'Installs/Configures redis, multi instace support'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name             'L7-redis'
-version          '1.0.4'
+version          '1.0.5'
 source_url       'https://github.com/szelcsanyi/chef-redis'
 issues_url       'https://github.com/szelcsanyi/chef-redis/issues'
 
 supports 'ubuntu', '>= 12.04'
 supports 'debian', '>= 7.0'
+
+depends 'cron'
