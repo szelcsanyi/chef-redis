6bc7e58ec03c42b5fb442e9f2698f243452e5631
Kevin Bonner <kbonner@notion.ai>
2016-02-17 02:39:54 +0000

    dynamically create the redis instance config file, convert custom instance defaults to cookbook default attributes, allow per-instance config options, support the old instance parameters

--- providers/pool.rb
+++ providers/pool.rb
@@ -9,6 +9,8 @@ def whyrun_supported?
 end
 
 action :remove do
+  inst_port = new_resource.port || node['L7-redis']['config']['port']
+
   service "redis-server-#{new_resource.name}" do
     action [:stop, :disable]
   end
@@ -25,26 +27,65 @@ action :remove do
     action :delete
   end
 
-  file "/tmp/redis-monitoring-status-#{new_resource.port}" do
+  file "/tmp/redis-monitoring-status-#{inst_port}" do
     action :delete
   end
-
 end
 
 action :create do
   Chef::Log.info("Redis pool: #{new_resource.name}")
 
+  inst_config = if new_resource.config.nil?
+                  node['L7-redis']['config']
+                else
+                  node['L7-redis']['config'].merge(new_resource.config)
+                end
+
+  Chef::Log.info("DEBUG: inst_config = #{inst_config.inspect}")
+
+  # support old resource settings
+  inst_config.override['L7-redis']['config']['port'] = new_resource.port unless new_resource.port.nil?
+  inst_config.override['L7-redis']['config']['bind'] = new_resource.bind unless new_resource.bind.nil?
+  inst_config.override['L7-redis']['config']['timeout'] = new_resource.timeout unless new_resource.timeout.nil?
+  inst_config.override['L7-redis']['config']['tcp_keepalive'] = new_resource.tcp_keepalive unless new_resource.tcp_keepalive.nil?
+  inst_config.override['L7-redis']['config']['loglevel'] = new_resource.loglevel unless new_resource.loglevel.nil?
+  inst_config.override['L7-redis']['config']['databases'] = new_resource.databases unless new_resource.databases.nil?
+  inst_config.override['L7-redis']['config']['dir'] = new_resource.datadir unless new_resource.datadir.nil?
+  # inst_config.override['L7-redis']['config']['maxmemory'] = new_resource.maxmemory unless new_resource.maxmemory.nil?
+  inst_config.override['L7-redis']['config']['maxmemory_policy'] = new_resource.maxmemory_policy unless new_resource.maxmemory_policy.nil?
+  inst_config.override['L7-redis']['config']['maxmemory_samples'] = new_resource.maxmemory_samples unless new_resource.maxmemory_samples.nil?
+  inst_config.override['L7-redis']['config']['slaveof'] = new_resource.slaveof unless new_resource.slaveof.nil?
+  inst_config.override['L7-redis']['config']['slave_read_only'] = new_resource.slave_read_only unless new_resource.slave_read_only.nil?
+  inst_config.override['L7-redis']['config']['stop_writes_on_bgsave_error'] = new_resource.stop_writes_on_bgsave_error unless new_resource.stop_writes_on_bgsave_error.nil?
+  inst_config.override['L7-redis']['config']['unixsocketperm'] = new_resource.unixsocketperm unless new_resource.unixsocketperm.nil?
+  inst_config.override['L7-redis']['config']['requirepass'] = new_resource.requirepass unless new_resource.requirepass.nil?
+  inst_config.override['L7-redis']['config']['masterauth'] = new_resource.masterauth unless new_resource.masterauth.nil?
+  inst_config.override['L7-redis']['config']['appendonly'] = new_resource.appendonly unless new_resource.appendonly.nil?
+  inst_config.override['L7-redis']['config']['appendfsync'] = new_resource.appendfsync unless new_resource.appendfsync.nil?
+  inst_config.override['L7-redis']['config']['no_appendfsync_on_rewrite'] = new_resource.no_appendfsync_on_rewrite unless new_resource.no_appendfsync_on_rewrite.nil?
+  inst_config.override['L7-redis']['config']['auto_aof_rewrite_percentage'] = new_resource.auto_aof_rewrite_percentage unless new_resource.auto_aof_rewrite_percentage.nil?
+  inst_config.override['L7-redis']['config']['auto_aof_rewrite_min_size'] = new_resource.auto_aof_rewrite_min_size unless new_resource.auto_aof_rewrite_min_size.nil?
+  inst_config.override['L7-redis']['config']['aof_rewrite_incremental_fsync'] = new_resource.aof_rewrite_incremental_fsync unless new_resource.aof_rewrite_incremental_fsync.nil?
+
+  # support old conditional definition of bgsave snapshot rules
+  if !new_resource.snapshotting.nil? && new_resource.snapshotting == True
+    inst_config.override['L7-redis']['config']['save'] = new_resource.snapshot_rules
+  elsif new_resource.snapshotting == False
+    inst_config.override['L7-redis']['config']['save'] = nil
+  end
+
+  # override values of per-instance settings if not configured
+  inst_config.override['L7-redis']['config']['pidfile'] = "/var/run/redis/redis-server-#{new_resource.name}.pid" if inst_config['L7-redis']['config']['pidfile'].nil?
+  inst_config.override['L7-redis']['config']['unixsocket'] = "/var/run/redis/redis-#{new_resource.name}.sock" if inst_config['L7-redis']['config']['unixsocket'].nil?
+  inst_config.override['L7-redis']['config']['logfile'] = "/var/log/redis/redis-server-#{new_resource.name}.log" if inst_config['L7-redis']['config']['logfile'].nil?
+  inst_config.override['L7-redis']['config']['dbfilename'] = "dump-#{new_resource.name}.rdb" if inst_config['L7-redis']['config']['dbfilename'].nil?
+  inst_config.override['L7-redis']['config']['appendfilename'] = "appendonly-#{new_resource.name}.aof" if inst_config['L7-redis']['config']['appendfilename'].nil?
+
   package 'redis-server' do
     action :install
   end
 
-  if new_resource.datadir.nil?
-    datadir = "/var/lib/redis-#{new_resource.name}"
-  else
-    datadir = new_resource.datadir
-  end
-
-  directory datadir do
+  directory inst_config['L7-redis']['config']['dir'] do
     action :create
     owner 'redis'
     group 'redis'
@@ -67,31 +108,7 @@ action :create do
     group 'root'
     mode '0644'
     variables(
-      name: new_resource.name,
-      port: new_resource.port,
-      bind: new_resource.bind,
-      timeout: new_resource.timeout,
-      tcp_keepalive: new_resource.tcp_keepalive,
-      loglevel: new_resource.loglevel,
-      databases: new_resource.databases,
-      datadir: datadir,
-      maxmemory: new_resource.maxmemory,
-      maxmemory_policy: new_resource.maxmemory_policy,
-      maxmemory_samples: new_resource.maxmemory_samples,
-      slaveof: new_resource.slaveof,
-      slave_read_only: new_resource.slave_read_only,
-      snapshotting: new_resource.snapshotting,
-      snapshot_rules: new_resource.snapshot_rules,
-      stop_writes_on_bgsave_error: new_resource.stop_writes_on_bgsave_error,
-      unixsocketperm: new_resource.unixsocketperm,
-      requirepass: new_resource.requirepass,
-      masterauth: new_resource.masterauth,
-      appendonly: new_resource.appendonly,
-      appendfsync: new_resource.appendfsync,
-      no_appendfsync_on_rewrite: new_resource.no_appendfsync_on_rewrite,
-      auto_aof_rewrite_percentage: new_resource.auto_aof_rewrite_percentage,
-      auto_aof_rewrite_min_size: new_resource.auto_aof_rewrite_min_size,
-      aof_rewrite_incremental_fsync: new_resource.aof_rewrite_incremental_fsync
+      config: inst_config
     )
     notifies :restart, "service[redis-server-#{new_resource.name}]", :delayed
   end
@@ -108,7 +125,7 @@ action :create do
     day '*'
     month '*'
     weekday '*'
-    command "if timeout 3 /usr/bin/redis-cli -s /var/run/redis/redis-#{new_resource.name}.sock -a '#{new_resource.requirepass}' INFO > /tmp/redis-monitoring-status-#{new_resource.port}.tmp; then sleep 1; mv /tmp/redis-monitoring-status-#{new_resource.port}.tmp /tmp/redis-monitoring-status-#{new_resource.port}; else rm -f /tmp/redis-monitoring-status-#{new_resource.port}.tmp; fi"
+    command "if timeout 3 /usr/bin/redis-cli -s /var/run/redis/redis-#{new_resource.name}.sock -a '#{inst_config['L7-redis']['config']['requirepass']}' INFO > /tmp/redis-monitoring-status-#{inst_config['L7-redis']['config']['port']}.tmp; then sleep 1; mv /tmp/redis-monitoring-status-#{inst_config['L7-redis']['config']['port']}.tmp /tmp/redis-monitoring-status-#{inst_config['L7-redis']['config']['port']}; else rm -f /tmp/redis-monitoring-status-#{inst_config['L7-redis']['config']['port']}.tmp; fi"
     user 'root'
     shell '/bin/bash'
   end
@@ -118,5 +135,4 @@ action :create do
     action [:stop, :disable]
     status_command '/etc/init.d/redis-server status'
   end
-
 end
